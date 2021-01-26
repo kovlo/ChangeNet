89dc47e2a1626d74e7238783405a4817774767f4
Kovács Lóránt <kovacslorant@gmail.com>
2021-01-26 15:48:07 +0100

    filename variables renamed

--- TrainValTxtMaker.py
+++ TrainValTxtMaker.py
@@ -2,13 +2,14 @@ import os
 from pathlib import Path
 import random
 
+train_file_list = './trainCD.txt'
+val_file_list =  './valCD.txt'
+test_file_list = './testCD.txt'
 
-traintxt = 'trainCD.txt'
-valtxt  = 'valCD.txt'
 ## Train Val txt
 rootdir = Path('/home/lorant/Projects/data/ChangeNet/combined/train/')
 
-with open(traintxt, 'w') as tr, open(valtxt, 'w') as vl:
+with open(train_file_list, 'w') as tr, open(val_file_list, 'w') as vl:
 
     images = os.listdir(rootdir)
     images.sort()
@@ -23,14 +24,13 @@ with open(traintxt, 'w') as tr, open(valtxt, 'w') as vl:
         savedirpath = Path(rootdir.parent.stem)/rootdir.stem
         a=str(savedirpath/gtimname)
         f.write(a+"\n")
-print('Done!')
+print('Train Val done!')
 
 ## Test txt
-testtxt = 'testCD.txt'
 
 rootdir = Path('/home/lorant/Projects/data/ChangeNet/combined/test/')
 
-with open(testtxt, 'w') as tst:
+with open(test_file_list, 'w') as tst:
    
     images = os.listdir(rootdir)
     images.sort()
@@ -42,4 +42,4 @@ with open(testtxt, 'w') as tst:
         savedirpath = Path(rootdir.parent.stem)/rootdir.stem
         a=str(savedirpath/gtimname)
         f.write(a+"\n")
-print('Done!')
+print('Test done!')
