9b8913adc22fa58f6803bc5c197fcf3c1623f05f
Kovács Lóránt <kovacslorant@gmail.com>
2021-02-15 14:49:01 +0100

    Update TrainValTxtMaker.py

--- TrainValTxtMaker.py
+++ TrainValTxtMaker.py
@@ -1,17 +1,19 @@
 import os
 from pathlib import Path
 import random
+import glob
 
 train_file_list = './trainCD.txt'
 val_file_list =  './valCD.txt'
 test_file_list = './testCD.txt'
 
 ## Train Val txt
-rootdir = Path('/home/lorant/Projects/data/ChangeNet/combined/train/')
+rootdir = Path('/home/lorant/Projects/data/Change3D/Change3D_dyn/')
 
 with open(train_file_list, 'w') as tr, open(val_file_list, 'w') as vl:
 
-    images = os.listdir(rootdir)
+    #images = os.listdir(rootdir)
+    images = glob.glob(str(rootdir)+'/*.png')
     images.sort()
     print(len(images))
 
@@ -28,11 +30,11 @@ print('Train Val done!')
 
 ## Test txt
 
-rootdir = Path('/home/lorant/Projects/data/ChangeNet/combined/test/')
+rootdir = Path('/home/lorant/Projects/data/Change3D/Change3D_stat_4class_cut_final/imgs_multi_dyn/')
 
 with open(test_file_list, 'w') as tst:
-   
-    images = os.listdir(rootdir)
+    #images = os.listdir(rootdir)
+    images = glob.glob(str(rootdir)+'/*.png')
     images.sort()
     print(len(images))
 
