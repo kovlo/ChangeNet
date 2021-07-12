43e0b24d6e958cb64d92e053468ff73d14399f8d
Kovács Lóránt <kovacslorant@gmail.com>
2021-07-12 14:06:05 +0200

    Update exploredataset.py

--- exploredataset.py
+++ exploredataset.py
@@ -16,127 +16,129 @@ import ipywidgets as widgets
 import pickle
 from PIL import ImageMath   
 
-train_file_list = './trainCD.txt'
-val_file_list =  './valCD.txt'
-test_file_list = './testCD.txt'
+def explore():
+    train_file_list = './trainCD.txt'
+    val_file_list =  './valCD.txt'
+    test_file_list = './testCD.txt'
 
-base_data_dir = ''
-train_pickle_file = './change_dataset_trainCD.pkl'
-val_pickle_file = './change_dataset_valCD.pkl'
-test_pickle_file = './change_dataset_testCD.pkl'
-
-
-validation_set = {}
-training_set = {}
-test_set = {}
-
-image_size  = 256
-image_sizeH = 128
-image_sizeW = 1024
-
-# #### Parse the path files
-
-# %%
-train_file_list = [line.rstrip('\n').split() for line in open(train_file_list)]
-val_file_list = [line.rstrip('\n').split() for line in open(val_file_list)]
-test_file_list = [line.rstrip('\n').split() for line in open(test_file_list)]
-print('Length Training Set:', len(train_file_list))
-print('Length Validation Set:', len(val_file_list))
-print('Length Test Set:', len(test_file_list))
-size_train = len(train_file_list)
-size_validation = len(val_file_list)
-size_test = len(test_file_list)
-
-def ReadImage(FileName):
-    # Load the reference, test and label images
-    trio_pil = Image.open(FileName).convert('I;16')
-
-    #reference_img = (trio_img[0:image_sizeH,:]/1000/42.42)
-    #reference_img = np.tile(reference_img[:,:,np.newaxis],(1,1,3))
-    reference_pil = trio_pil.crop(box=(0,0*image_sizeH,image_sizeW,1*image_sizeH)).convert('F')
-
-    #test_img = trio_img[image_sizeH:2*image_sizeH,:]/1000/42.42
-    #test_img = np.tile(test_img[:,:,np.newaxis],(1,1,3))
-    test_pil = trio_pil.crop(box=(0,1*image_sizeH,image_sizeW,2*image_sizeH)).convert('F')
-
-    label_pil = trio_pil.crop(box=(0,2*image_sizeH,image_sizeW,3*image_sizeH)).convert("L")
-
-    reference_pil = ImageMath.eval("(x/1000/42.42*255)",x=reference_pil).convert('L')
-    test_pil = ImageMath.eval("(x/1000/42.42*255)",x=test_pil).convert('L')
-    label_pil = Image.eval(label_pil,lambda x: 1 if x>0 else 0)
-
-    return reference_pil,test_pil,label_pil
+    base_data_dir = ''
+    train_pickle_file = './change_dataset_trainCD.pkl'
+    val_pickle_file = './change_dataset_valCD.pkl'
+    test_pickle_file = './change_dataset_testCD.pkl'
 
 
-# #### Load Validation Set On Memory
-# %%
-idx2 = 0
-plt.figure
-for idx, entry in enumerate(tqdm(val_file_list[:2000])):
-    reference_pil,test_pil,label_pil= ReadImage(base_data_dir + entry[0])
+    validation_set = {}
+    training_set = {}
+    test_set = {}
 
-    # Populate validation dictionary with tupple (reference,test,label)
-    for i in range(0,8):
-        # Populate training dictionary with tupple (reference,test,label)
-        reference_pilPart = reference_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
-        test_pilPart=test_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
-        label_pilPart=label_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+    image_size  = 256
+    image_sizeH = 128
+    image_sizeW = 1024
 
-        validation_set[idx2] = reference_pilPart,test_pilPart,label_pilPart   
-        reference_pilPart,test_pilPart,label_pilPart=0,0,0
-        idx2+=1
+    # #### Parse the path files
 
-print('Saving Pickle Validation Set')
-with open(val_pickle_file, 'wb') as handle:
-    pickle.dump(validation_set, handle, protocol=4)
-validation_set={}
-print('Done')
+    # %%
+    #train_file_list = [line.rstrip('\n').split() for line in open(train_file_list)]
+    #val_file_list = [line.rstrip('\n').split() for line in open(val_file_list)]
+    test_file_list = [line.rstrip('\n').split() for line in open(test_file_list)]
+    #print('Length Training Set:', len(train_file_list))
+    #print('Length Validation Set:', len(val_file_list))
+    print('Length Test Set:', len(test_file_list))
+    #size_train = len(train_file_list)
+    #size_validation = len(val_file_list)
+    size_test = len(test_file_list)
 
-# #### Load Training Set On Memory
-idx2 = 0
-for idx, entry in enumerate(tqdm(train_file_list[:10000])):
-    reference_pil,test_pil,label_pil= ReadImage(base_data_dir + entry[0])
+    def ReadImage(FileName):
+        # Load the reference, test and label images
+        trio_pil = Image.open(FileName).convert('I;16')
 
-    # Populate validation dictionary with tupple (reference,test,label)
-    for i in range(0,8):
-        # Populate training dictionary with tupple (reference,test,label)
-        reference_pilPart = reference_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
-        test_pilPart=test_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
-        label_pilPart=label_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+        #reference_img = (trio_img[0:image_sizeH,:]/1000/42.42)
+        #reference_img = np.tile(reference_img[:,:,np.newaxis],(1,1,3))
+        reference_pil = trio_pil.crop(box=(0,0*image_sizeH,image_sizeW,1*image_sizeH)).convert('F')
 
-        training_set[idx2] = reference_pilPart,test_pilPart,label_pilPart   
-        idx2+=1
- 
-print('Saving Pickle Training Set')
-with open(train_pickle_file, 'wb') as handle:
-    pickle.dump(training_set, handle, protocol=4)
-print('Saved.')
-training_set={}
-# #### Load Test Set On Memory
-idx2 = 0
-for idx, entry in enumerate(tqdm(test_file_list)):
-    reference_pil,test_pil,label_pil= ReadImage(base_data_dir + entry[0])
+        #test_img = trio_img[image_sizeH:2*image_sizeH,:]/1000/42.42
+        #test_img = np.tile(test_img[:,:,np.newaxis],(1,1,3))
+        test_pil = trio_pil.crop(box=(0,1*image_sizeH,image_sizeW,2*image_sizeH)).convert('F')
 
-    # Populate validation dictionary with tupple (reference,test,label)
-    for i in range(0,8):
-        # Populate training dictionary with tupple (reference,test,label)
-        reference_pilPart = reference_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
-        test_pilPart=test_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
-        label_pilPart=label_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+        label_pil = trio_pil.crop(box=(0,2*image_sizeH,image_sizeW,3*image_sizeH)).convert("L")
 
-        test_set[idx2] = reference_pilPart,test_pilPart,label_pilPart  
-        """
-        plt.subplot(3,8,i+1)
-        plt.imshow(reference_pilPart)
-        plt.subplot(3,8,i+1+8)
-        plt.imshow(test_pilPart)
-        plt.subplot(3,8,i+1+8+8)
-        plt.imshow(label_pilPart)
-        """
-        idx2+=1
-    #plt.show()
-print('Saving Pickle Training Set')
-with open(test_pickle_file, 'wb') as handle:
-    pickle.dump(test_set, handle, protocol=4)
-print('Saved.')
-test_set={}
+        reference_pil = ImageMath.eval("(x/1000/42.42*255)",x=reference_pil).convert('L')
+        test_pil = ImageMath.eval("(x/1000/42.42*255)",x=test_pil).convert('L')
+        label_pil = Image.eval(label_pil,lambda x: 1 if x>0 else 0)
+
+        return reference_pil,test_pil,label_pil
+
+    """
+    # #### Load Validation Set On Memory
+    # %%
+    idx2 = 0
+    plt.figure
+    for idx, entry in enumerate(tqdm(val_file_list[:2000])):
+        reference_pil,test_pil,label_pil= ReadImage(base_data_dir + entry[0])
+
+        # Populate validation dictionary with tupple (reference,test,label)
+        for i in range(0,8):
+            # Populate training dictionary with tupple (reference,test,label)
+            reference_pilPart = reference_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+            test_pilPart=test_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+            label_pilPart=label_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+
+            validation_set[idx2] = reference_pilPart,test_pilPart,label_pilPart   
+            reference_pilPart,test_pilPart,label_pilPart=0,0,0
+            idx2+=1
+
+    print('Saving Pickle Validation Set')
+    with open(val_pickle_file, 'wb') as handle:
+        pickle.dump(validation_set, handle, protocol=4)
+    validation_set={}
+    print('Done')
+
+    # #### Load Training Set On Memory
+    idx2 = 0
+    for idx, entry in enumerate(tqdm(train_file_list[:10000])):
+        reference_pil,test_pil,label_pil= ReadImage(base_data_dir + entry[0])
+
+        # Populate validation dictionary with tupple (reference,test,label)
+        for i in range(0,8):
+            # Populate training dictionary with tupple (reference,test,label)
+            reference_pilPart = reference_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+            test_pilPart=test_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+            label_pilPart=label_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+
+            training_set[idx2] = reference_pilPart,test_pilPart,label_pilPart   
+            idx2+=1
+    
+    print('Saving Pickle Training Set')
+    with open(train_pickle_file, 'wb') as handle:
+        pickle.dump(training_set, handle, protocol=4)
+    print('Saved.')
+    training_set={}
+    """
+    # #### Load Test Set On Memory
+    idx2 = 0
+    for idx, entry in enumerate(tqdm(test_file_list)):
+        reference_pil,test_pil,label_pil= ReadImage(base_data_dir + entry[0])
+
+        # Populate validation dictionary with tupple (reference,test,label)
+        for i in range(0,8):
+            # Populate training dictionary with tupple (reference,test,label)
+            reference_pilPart = reference_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+            test_pilPart=test_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+            label_pilPart=label_pil.crop(box=(i*image_sizeH,0,image_sizeH*(i+1),image_sizeH))
+
+            test_set[idx2] = reference_pilPart,test_pilPart,label_pilPart  
+            """
+            plt.subplot(3,8,i+1)
+            plt.imshow(reference_pilPart)
+            plt.subplot(3,8,i+1+8)
+            plt.imshow(test_pilPart)
+            plt.subplot(3,8,i+1+8+8)
+            plt.imshow(label_pilPart)
+            """
+            idx2+=1
+        #plt.show()
+    print('Saving Pickle Training Set')
+    with open(test_pickle_file, 'wb') as handle:
+        pickle.dump(test_set, handle, protocol=4)
+    print('Saved.')
+    test_set={}
